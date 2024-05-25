c95ffd24f96349058ff1af2666533f5667018fd5
sarjeet singh <singh@SARJEETSINGH>
2024-05-25 15:00:29 +0530

    Shell , Flyout,Tab,Static Resource, AppConstants for static propety value use

---
+++ FirstDemoAppMAUI/Resources/AppConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemoAppMAUI
+{
+    static class AppConstants
+    {
+        public static readonly Color BackgroundColor = Colors.Aqua;
+        public static readonly Color ForegroundColor = Colors.Brown;
+    }
+}
